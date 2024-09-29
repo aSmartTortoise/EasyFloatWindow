775b6107839d9255b14216992401e24347a41d87
王永杰 <h-wangyongj@voyah.com.cn>
2024-09-29 10:59:54 +0800

    学习easyFloat，自定义window的 view。

--- example/src/main/java/com/lzf/easyfloat/example/activity/WindowBlurActivity.kt
+++ example/src/main/java/com/lzf/easyfloat/example/activity/WindowBlurActivity.kt
@@ -1,17 +1,64 @@
 package com.lzf.easyfloat.example.activity
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
+import com.lzf.easyfloat.EasyFloat
+import com.lzf.easyfloat.enums.ShowPattern
+import com.lzf.easyfloat.enums.SidePattern
 import com.lzf.easyfloat.example.BlurWindowHelper
 import com.lzf.easyfloat.example.R
+import com.lzf.easyfloat.interfaces.OnTouchRangeListener
+import com.lzf.easyfloat.utils.DragUtils
+import com.lzf.easyfloat.widget.BaseSwitchView
 
-class WindowBlurActivity : AppCompatActivity() {
+class WindowBlurActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window_blur)
         findViewById<Button>(R.id.btn_show_window).setOnClickListener {
             BlurWindowHelper(this).showWindow()
         }
+
+        findViewById<Button>(R.id.btn_show_easy_float).setOnClickListener {
+            showAppFloat()
+        }
+    }
+
+    private fun showAppFloat() {
+        EasyFloat.with(this.applicationContext)
+            .setShowPattern(ShowPattern.ALL_TIME)
+            .setSidePattern(SidePattern.RESULT_SIDE)
+            .setImmersionStatusBar(true)
+            .setGravity(Gravity.CENTER_HORIZONTAL, 0, 100)
+            .setLayout(R.layout.float_app_blur) {
+                it.findViewById<ImageView>(R.id.ivClose).setOnClickListener {
+                    EasyFloat.dismiss()
+                }
+            }
+            .registerCallback {
+                drag { _, motionEvent ->
+                    DragUtils.registerDragClose(motionEvent, object : OnTouchRangeListener {
+                        override fun touchInRange(inRange: Boolean, view: BaseSwitchView) {
+                            setVibrator(inRange)
+                            view.findViewById<TextView>(R.id.tv_delete).text =
+                                if (inRange) "松手删除" else "删除浮窗"
+
+                            view.findViewById<ImageView>(R.id.iv_delete)
+                                .setImageResource(
+                                    if (inRange) R.drawable.icon_delete_selected
+                                    else R.drawable.icon_delete_normal
+                                )
+                        }
+
+                        override fun touchUpInRange() {
+                            EasyFloat.dismiss()
+                        }
+                    }, showPattern = ShowPattern.ALL_TIME)
+                }
+            }
+            .show()
     }
 }
